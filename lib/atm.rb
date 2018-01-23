3450276d5f5757e9cb74b61b0e414cbee40cdc8f
Holger_Tidemand <tidemand.holger@gmail.com>
2018-01-23 09:45:44 +0100

    initialize balance and account allow withdraws from account

--- lib/atm.rb
+++ lib/atm.rb
@@ -3,7 +3,15 @@ attr_accessor :funds
   def initialize
     @funds = 1000
   end
-  def withdraw(amount)
-    @funds -= amount
+  def withdraw(amount, account)
+
+    case
+    when amount > account.balance
+      return
+    else
+      @funds -= amount
+      account.balance -= amount
+      {status: true, message: 'success', date: Date.today, amount: amount}
+    end
   end
 end
