450f31017a5b75c42c2bfb1dc634a3599ae22f81
Holger_Tidemand <tidemand.holger@gmail.com>
2018-01-23 15:19:47 +0100

    WIP account owner

--- lib/account.rb
+++ lib/account.rb
@@ -4,16 +4,26 @@ class Account
   attr_accessor :pin_code, :balance, :account_status, :exp_date
   STANDARD_VALIDITY_YRS = 5
 
-def initialize(owner, balance)
-  @owner = owner
-  @balance = balance
-  @account_status = :active
-  @pin_code = rand(1000..9999)
-  @exp_date = set_expiry_date
-end
+  def initialize()
+    @balance = 0
+    @account_status = :active
+    @pin_code = generate_pin()
+    @exp_date = set_expiry_date()
+  end
 
-def set_expiry_date
-  Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime("%m/%y")
-end
+  def set_expiry_date()
+    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime("%m/%y")
+  end
+
+  def generate_pin()
+    rand(1000..9999)
+  end
+
+  def deactivate()
+    # we chose to use an instance method over a class method because the state
+    # of the the activation is only valid once an instance of the the class has
+    # been created
+    @account_status = :inactive
+  end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,18 +4,37 @@ include Math
 
 
 describe Account do
-before do
-@subject = Account.new('holger', 1000)
-end
+  let(:person) { instance_double('Person', name: 'Jade') }
+
+  before do
+    # @subject = Account.new('holger', 1000)
+    subject {described_class.new({owner: person})}
+  end
+
+  it 'expacted to have a owner' do
+    expect(subject.owner).to eq person
+  end
 
   it 'checks length of pin code' do
-    number = @subject.pin_code
+    number = subject.pin_code
     number_length = Math.log10(number).to_i + 1
     expect(number_length).to eq 4
   end
 
   it 'is expected to have an expiry date on initialize' do
     expected_date = Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime("%m/%y")
-    expect(@subject.exp_date).to eq expected_date
+    expect(subject.exp_date).to eq expected_date
+  end
+
+  it 'is expected to have :active status on initialize' do
+    expect(subject.account_status).to eq :active
+  end
+
+  it 'is expected to :deactivate using a instance method' do
+    # we chose to use an instance method over a class method because the state
+    # of the the activation is only valid once an instance of the the class has
+    # been created
+    subject.deactivate()
+    expect(subject.account_status).to eq :inactive
   end
 end
