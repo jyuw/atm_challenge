dc16f0739c081cfa7fc19b16b0647e87af23152b
Jade <jywfang@gmail.com>
2018-01-23 14:07:26 +0100

    added expiry date

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,19 @@
 require 'date'
 # Account class
 class Account
-attr_accessor :pin_code, :balance, :account_status, :exp_date
+  attr_accessor :pin_code, :balance, :account_status, :exp_date
+  STANDARD_VALIDITY_YRS = 5
 
 def initialize(owner, balance)
   @owner = owner
   @balance = balance
   @account_status = :active
   @pin_code = rand(1000..9999)
+  @exp_date = set_expiry_date
+end
+
+def set_expiry_date
+  Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime("%m/%y")
 end
 
 end
