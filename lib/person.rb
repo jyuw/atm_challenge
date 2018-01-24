aa9c11c3b2d6e50f245a899b52c025774c85b7bc
Holger_Tidemand <tidemand.holger@gmail.com>
2018-01-24 10:11:26 +0100

    Person class added ability to create a account on initialize

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+# Person class
+require 'account.rb'
+class Person
+  attr_accessor :name, :cash, :account
+
+  def initialize(attrs = {})
+    @name = set_name(attrs[:name])
+    @cash = 0
+    @account = nil
+  end
+
+  def create_account()
+    @account = Account.new(owner: self)
+  end
+
+private
+
+  def set_name(obj)
+    obj == nil ? missing_name : @name = obj
+  end
+
+  def missing_name()
+    raise 'A name is required'
+  end
+
+
+end
---
+++ spec/person_spec.rb
@@ -0,0 +1,36 @@
+require './lib/person.rb'
+require './lib/atm.rb'
+
+describe Person do
+  subject { described_class.new(name: 'Thomas') }
+
+  it 'is expected to have a :name on initialize' do
+    expect(subject.name).not_to be nil
+  end
+
+  it 'is expected to raise error if no name is set' do
+    expect { described_class.new }.to raise_error 'A name is required'
+  end
+
+  it 'is expected to have a :cash attribute with value of 0 on initialize' do
+    expect(subject.cash).to eq 0
+  end
+
+  it 'is expected to have a :account attribute' do
+   expect(subject.account).to be nil
+  end
+
+  describe 'can create an Account' do
+    # As a Person,
+    # in order to be able to use banking services to manage my funds,
+    # i would like to be able to create a bank account
+    before { subject.create_account }
+    it 'of Account class ' do
+      expect(subject.account).to be_an_instance_of Account
+    end
+
+    it 'with himself as an owner' do
+      expect(subject.account.owner).to be subject
+    end
+  end
+end
